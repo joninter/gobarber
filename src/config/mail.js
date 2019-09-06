cc0eb20e153e8689af294b507e758152749ff9ce
Jonatan Machado <jonatanmachado35@gmail.com>
2019-09-06 12:18:33 -0300

    pack de funcionalidades feitas

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e679d07f86ebf2',
+    pass: '049c1cb9c2f37c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
